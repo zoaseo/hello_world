e76da438b56705976905fa31c55cc508dafe0906
zoaseo <101240239+zoaseo@users.noreply.github.com>
2023-01-12 10:54:55 +0900

    upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text('Hello World',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          )),
+        ),
+      ),
+    ),
+  );
+}
+
